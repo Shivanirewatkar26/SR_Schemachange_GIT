5eb4f22f79e5094d8370a43fc30b7133abb529c0
Shivanirewatkar26 <rewatkarshivani@gmail.com>
2025-10-19 10:16:45 +0800

    new store proc

---
+++ migrations/store_proc/V1.7._SP_object.sql
@@ -0,0 +1,17 @@
+CREATE OR REPLACE PROCEDURE Z_database_{{ENV}}.Z_Schema.INSERT_CUSTOMER(
+    IN_CUST_ID VARCHAR,
+    IN_CUST_NAME VARCHAR
+)
+RETURNS STRING
+LANGUAGE SQL
+AS
+$$
+BEGIN
+    -- Insert a new customer into the environment-specific table
+    INSERT INTO Z_database_{{ENV}}.Z_Schema.CUSTOMERS (CUSTOMER_ID, CUSTOMER_NAME)
+    VALUES (IN_CUST_ID, IN_CUST_NAME);
+    
+    -- Return success message
+    RETURN 'Customer ' || IN_CUST_ID || ' inserted successfully';
+END;
+$$;
